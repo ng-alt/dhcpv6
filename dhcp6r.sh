7d6fb812c17b0ad943e9c54532e729c9c3ff5d08
dlc-atl <dlc-atl>
2007-11-08 21:52:35 +0000

    Init scripts standards compliance fixes.

--- dhcp6r.sh
+++ dhcp6r.sh
@@ -1,89 +1,77 @@
-#!/bin/sh
-#
-# dhcp6r        dhcp6r is an implementation of DHCPv6 relay agent.
-#               This shell script takes care of starting and stopping
-#               dhcp6r.
+#!/bin/bash
 #
 # chkconfig: - 66 36
-# description: dhcp6r supports the DHCPv6 relay agent protocol. 
-#            
+# description: dhcp6r supports the DHCPv6 relay agent protocol.
 # processname: dhcp6r
 # config: /etc/sysconfig/dhcp6r
 
-# Source function library.
-. /etc/rc.d/init.d/functions
+# Source function library
+. /etc/init.d/functions
 
-# Source networking configuration.
-. /etc/sysconfig/network
-. /etc/sysconfig/dhcp6r
+RETVAL=0
+prog=dhcp6r
+dhcp6r=/usr/sbin/dhcp6r
+lockfile=/var/lock/subsys/dhcp6r
 
 # Check that networking is up.
 # networking is not up, return 1 for generic error
-[ ${NETWORKING} = "no" ] && exit 1
-
-# Check that files exist
-# return 5 if program is not installed
-[ -f /usr/sbin/dhcp6r ] || exit 5
-
-# return 6 if program is not configured
-[ -f /etc/sysconfig/dhcp6r ] || exit 6
-
-RETVAL=0
-prog="dhcp6r"
+. /etc/sysconfig/network
+[ $NETWORKING = "no" ] && exit 1
 
 start() {
-	# Start daemons.
-	echo -n $"Starting $prog: "
-	daemon /usr/sbin/dhcp6r ${DHCP6RARGS}
-	RETVAL=$?
-	echo
-	[ $RETVAL -eq 0 ] && touch /var/lock/subsys/dhcp6r
-	return $RETVAL
+    # return 5 if program is not installed
+    [ -x $dhcp6r ] || exit 5
+
+    # return 6 if program is not configured
+    [ -f /etc/sysconfig/dhcp6r ] || exit 6
+    . /etc/sysconfig/dhcp6r
+
+    echo -n $"Starting $prog: "
+    daemon $dhcp6r $DHCP6RARGS
+    RETVAL=$?
+    echo
+    [ $RETVAL -eq 0 ] && touch $lockfile
+    return $RETVAL
 }
 
 stop() {
-	# Stop daemons.
-	echo -n $"Shutting down $prog: "
-	killproc dhcp6r
-	RETVAL=$?
-	echo
-	[ $RETVAL -eq 0 ] && rm -f /var/lock/subsys/dhcp6r
-	return $RETVAL
+    echo -n $"Shutting down $prog: "
+    killproc $prog -TERM
+    RETVAL=$?
+    echo
+    [ $RETVAL -eq 0 ] && rm -f $lockfile
+    return $RETVAL
 }
 
 # See how we were called.
 case "$1" in
-  start)
-	start
-	RETVAL=$?
-	;;
-  stop)
-	stop
-	RETVAL=$?
-	;;
-  restart|force-reload)
-	stop
-	start
-	RETVAL=$?
-	;;
-  reload)
-	# unimplemented
-	RETVAL=3
-	;;
-  condrestart)
-	if [ -f /var/lock/subsys/dhcp6s ]; then
-	    stop
-	    start
-	    RETVAL=$?
-	fi
-	;;
-  status)
-	status dhcp6r
-	RETVAL=$?
-	;;
-  *)
-	echo $"Usage: $0 {start|stop|restart|condrestart|status}"
-	exit 1
+    start)
+        start
+        ;;
+    stop)
+        stop
+        ;;
+    restart)
+        stop
+        start
+        ;;
+    reload)
+        RETVAL=3
+        ;;
+    condrestart)
+        if [ -f $lockfile ]; then
+            stop
+            start
+        fi
+        ;;
+    status)
+        status dhcp6r
+        RETVAL=$?
+        ;;
+    *)
+        echo $"Usage: $0 {start|stop|restart|condrestart|status}"
+        RETVAL=3
+        ;;
 esac
 
 exit $RETVAL
--- dhcp6s.sh
+++ dhcp6s.sh
@@ -1,94 +1,84 @@
-#!/bin/sh
-#
-# dhcp6s        dhcp6s is an implementation of DHCPv6 server.
-#               This shell script takes care of starting and stopping
-#               dhcp6s.
+#!/bin/bash
 #
 # chkconfig: - 66 36
-# description: dhcp6s supports server side of  Dynamic Host Configuration \
+# description: dhcp6s supports the server side of Dynamic Host Configuration \
 #              Protocol for IPv6.
 # processname: dhcp6s
 # config: /etc/dhcp6s.conf
 # config: /etc/sysconfig/dhcp6s
 
-# Source function library.
-. /etc/rc.d/init.d/functions
+# Source function library
+. /etc/init.d/functions
 
-# Source networking configuration.
-. /etc/sysconfig/network
-. /etc/sysconfig/dhcp6s
+RETVAL=0
+prog=dhcp6s
+dhcp6s=/usr/sbin/dhcp6s
+lockfile=/var/lock/subsys/dhcp6s
 
 # Check that networking is up.
 # networking is not up, return 1 for generic error
-[ ${NETWORKING} = "no" ] && exit 1
-
-# Check that files exist
-# return 5 if program is not installed
-[ -x /usr/sbin/dhcp6s ] || exit 5
-
-# return 6 if program is not configured
-[ -f /etc/dhcp6s.conf ] || exit 6
-
-if [ "x$DHCP6SIF" =  "x" ]; then
-	logger -s -t "dhcp6s" -p "daemon.info" "Warning: dhcp6s listening on ALL interfaces - set DHCP6SIF in /etc/sysconfig/dhcp6s"
-fi
-
-RETVAL=0
-prog="dhcp6s"
+. /etc/sysconfig/network
+[ $NETWORKING = "no" ] && exit 1
 
 start() {
-	# Start daemons.
-	echo -n $"Starting $prog: "
-	daemon /usr/sbin/dhcp6s -c /etc/dhcp6s.conf ${DHCP6SARGS} ${DHCP6SIF}
-	RETVAL=$?
-	echo
-	[ $RETVAL -eq 0 ] && touch /var/lock/subsys/dhcp6s
-	return $RETVAL
+    # return 5 if program is not installed
+    [ -x $dhcp6s ] || exit 5
+
+    # return 6 if program is not configured
+    [ -f /etc/dhcp6s.conf ] || exit 6
+    [ -f /etc/sysconfig/dhcp6s ] || exit 6
+    . /etc/sysconfig/dhcp6s
+
+    if [ -z "$DHCP6SIF" ]; then
+        logger -s -t "$prog" -p "daemon.info" "Warning: $prog listening on ALL interfaces"
+    fi
+
+    echo -n $"Starting $prog: "
+    daemon $dhcp6s -c /etc/dhcp6s.conf $DHCP6SARGS $DHCP6SIF
+    RETVAL=$?
+    echo
+    [ $RETVAL -eq 0 ] && touch $lockfile
+    return $RETVAL
 }
 
 stop() {
-	# Stop daemons.
-	echo -n $"Shutting down $prog: "
-	killproc dhcp6s
-	RETVAL=$?
-	echo
-	[ $RETVAL -eq 0 ] && rm -f /var/lock/subsys/dhcp6s
-	return $RETVAL
+    echo -n $"Shutting down $prog: "
+    killproc $prog -TERM
+    RETVAL=$?
+    echo
+    [ $RETVAL -eq 0 ] && rm -f $lockfile
+    return $RETVAL
 }
 
 # See how we were called.
 case "$1" in
-  start)
-	start
-	RETVAL=$?
-	;;
-  stop)
-	stop
-	RETVAL=$?
-	;;
-  restart|force-reload)
-	stop
-	start
-	RETVAL=$?
-	;;
-  reload)
-	# unimplemented
-	RETVAL=3
-	;;
-  condrestart)
-	if [ -f /var/lock/subsys/dhcp6s ]; then
-	    stop
-	    start
-	    RETVAL=$?
-	fi
-	;;
-  status)
-	status dhcp6s
-	RETVAL=$?
-	;;
-  *)
-	echo $"Usage: $0 {start|stop|restart|condrestart|status}"
-	exit 1
+    start)
+        start
+        ;;
+    stop)
+        stop
+        ;;
+    restart)
+        stop
+        start
+        ;;
+    reload)
+        RETVAL=3
+        ;;
+    condrestart)
+        if [ -f $lockfile ]; then
+            stop
+            start
+        fi
+        ;;
+    status)
+        status $prog
+        RETVAL=$?
+        ;;
+    *)
+        echo $"Usage: $0 {start|stop|restart|condrestart|status}"
+        RETVAL=3
+        ;;
 esac
 
 exit $RETVAL
