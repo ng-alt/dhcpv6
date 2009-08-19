b3c07b65537acd461936030e14bbf0ddbe3f5bae
David Cantrell <dcantrell@redhat.com>
2009-08-19 13:23:23 -1000

    Add license boilerplates to all .c and .h files.
    We're a derived work now, so everything is LGPL.  Keep existing
    boilerplates, but add new ones.

--- include/confdata.h
+++ include/confdata.h
@@ -1,3 +1,24 @@
+/*
+ * confdata.h
+ *
+ * Copyright (C) 2009  Red Hat, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author(s): David Cantrell <dcantrell@redhat.com>
+ */
+
 /* ported from KAME: config.h,v 1.18 2002/06/14 15:32:55 jinmei Exp */
 
 /*
